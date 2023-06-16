2a30d81592b8dcb5ee9908162d44a219b86b44da
Anyanka2 <123205369+Anyanka2@users.noreply.github.com>
2023-06-16 23:03:18 +0200

    finished task

---
+++ src/references-api.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://api.thecatapi.com/v1/';
+const ALLBREEDS_ENDPOINT = 'breeds';
+const SEARCH_ENDPOINT = 'images/search';
+const API_KEY =
+  'live_eARvrURDRZtFPS6NbdoLysWK9OuRguNrcnkD0q9fkZ9Ys0C8qUlhsdmJV6MrHsGH';
+
+export { BASE_URL, API_KEY, ALLBREEDS_ENDPOINT, SEARCH_ENDPOINT };
